7d1b3b915be6e09016dd2db59f02cc64b25d3f2c
sky.red2212@gmail.com <Blockstacksdottech>
2023-11-16 04:04:32 +0100

    update url

--- constants/apiInfo.ts
+++ constants/apiInfo.ts
@@ -1,5 +1,5 @@
 //export const BASE_DOMAIN = "http://localhost:3030"
-//export const BASE_DOMAIN = "https://ampouletude.com"
-export const BASE_DOMAIN = "http://localhost"
+export const BASE_DOMAIN = "https://ampouletude.com"
+//export const BASE_DOMAIN = "http://localhost"
 //export const BASE_URL = BASE_DOMAIN + "/api/"
 export const BASE_URL = BASE_DOMAIN + "/backendapi/"
